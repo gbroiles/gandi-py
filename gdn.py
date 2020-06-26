9ef6b86a9e172c0f0f62dd387012ba79e842724d
gbroiles <gbroiles@gmail.com>
2020-06-26 14:00:04 -0700

    seems to be working.

--- gdn.py
+++ gdn.py
@@ -1,14 +1,27 @@
-#!/usr/bin/python3
-
+import os
+import sys
 import requests
 
+URL = "https://api.gandi.net/v5/domain/check"
 
-url = "https://api.gandi.net/v5/domain/check"
+try:
+    apikey = os.environ["GANDI_API"]
+except KeyError:
+    print("Environment variable GANDI_API must be set.")
+    sys.exit(1)
 
-querystring = {"name":"example.com","processes":["create","transfer"],"grid":"C"}
+querystring = {
+    "name": sys.argv[1],
+}
 
-headers = {'authorization': 'Apikey your-api-key'}
+headers = {"authorization": "Apikey " + apikey}
 
-response = requests.request("GET", url, headers=headers, params=querystring)
+try:
+    response = requests.request("GET", URL, headers=headers, params=querystring)
+    response.raise_for_status()
+except Exception as err:
+    print(f"Error occurred: {err}")
 
-print(response.text)
+json_data = response.json()
+products = json_data.get("products")
+print("{} is {}".format(sys.argv[1], products[0].get("status")))
