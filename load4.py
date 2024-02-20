afb9a6c6da7f6430cb6de86b4d9e962a76d01cae
Greg Broiles <gbroiles@gmail.com>
2024-02-20 15:41:13 -0800

    update

---
+++ load4.py
@@ -0,0 +1,31 @@
+import time
+import sqlite3
+
+def char_range(c1, c2):
+    for c in range(ord(c1), ord(c2)+1):
+        yield chr(c)
+
+database = "domains.db"
+
+con = sqlite3.connect(database)
+
+cur = con.cursor()
+
+data = []
+
+for c1 in char_range("a","z"):
+    for c2 in char_range("a","z"):
+        for c3 in char_range("a","z"):
+            for c4 in char_range("a","z"):
+                name = c1+c2+c3+c4
+                timestamp=time.time()
+                data.append((name+".com",'unknown',timestamp))
+                data.append((name+".net",'unknown',timestamp))
+                data.append((name+".org",'unknown',timestamp))
+
+#for item in data:
+#    print(item)
+
+cur.executemany("INSERT INTO domains VALUES (?, ?, ?)",data)
+con.commit()
+
